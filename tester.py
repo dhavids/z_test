b3c27e0e45c296a2a41c7a65af56b591d815c895
dhavids <agunloyeayomide@gmail.com>
2024-04-18 14:03:11 +0100

    Added an ipynb notebook tester

--- tester.py
+++ tester.py
@@ -1,4 +1,5 @@
 from util.util import *
+import yaml
 
 base= "/home/ayomide/OneDrive/MARL_git/z_tester/test"
 name = get_file_name(base, "temp", ext=".png")
@@ -7,3 +8,9 @@ info = (name, "py")
 
 test = " ".join(info)
 print(test)
+
+path = "/home/ayomide/OneDrive/MARL_git/configs/mpe/mappo/train.yaml"
+with open(path, 'r') as f:
+    train_config = yaml.safe_load(f)
+
+print(recursive_update(train_config['agg'], train_config['default']))
